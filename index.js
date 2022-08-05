45922b043abe026f096e3a6484fdb25a85f0b4cd
digoe874 <digoe87@gmail.com>
2022-08-05 13:50:04 -0500

    some changes

---
+++ index.js
@@ -0,0 +1,26 @@
+// https://www.omdbapi.com/?i=tt3896198&apikey=a57f6202
+
+// console.log(fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a57f6202&s=anime"))
+const moviesListEl = document.querySelector('.movie-list')
+
+
+async function main () {
+    const movies = await fetch('http://www.omdbapi.com/?apikey=a57f6202&s=anime')
+    const moviesData = await movies.json();
+    moviesListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).slice(0,6).join("");
+}
+
+main();
+
+function movieHTML(movie) {
+  return `<div class="movie">
+  <div class="movie-card">
+    <div class="movie-card__container">
+        <img src="${movie.poster}" alt="">
+      <h3>${movie.title}</h4>
+        <p><b>Type:</b>${movie.type}</p>
+        <p><b>Year:</b>${movie.title}</p>
+    </div>
+  </div>
+</div>`;
+}
